06b749b997bd4869f7e8adaa0891a9de2872913e
이건희 <leech9876@naver.com>
2024-09-27 16:17:36 +0900

    #5 - style : 자바 코딩 컨벤션에 따라 매개변수 사이에 띄어쓰기 적용

--- src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
@@ -3,5 +3,5 @@ package com.fastcampus.projectboard.repository;
 import com.fastcampus.projectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment,Long> {
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -3,5 +3,5 @@ package com.fastcampus.projectboard.repository;
 import com.fastcampus.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticleRepository extends JpaRepository<Article,Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
