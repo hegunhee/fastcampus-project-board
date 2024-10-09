29311a35e238beb72dce3e4eb66825a84f43916f
hegunhee <gunhee0072@gmail.com>
2024-10-09 10:42:56 +0900

    #17 - fix : UserAccount클래스 AuditingFields 상속,EntityListener 삭제
    #10 에서 UserAccount에서 AuditingFields 사항을 적용하려 했으나
    해당 브랜치에서 @EntityListener 코드를 추가해서  #10에서 AudiingFields 변경 커밋을 체리픽하고 @EntityListener를 삭제했습니다.

--- src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
+++ src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
@@ -3,14 +3,14 @@ package com.fastcampus.projectboard.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Index;
+import javax.persistence.Entity;
 import java.util.Objects;
 
 @Getter
@@ -21,9 +21,8 @@ import java.util.Objects;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class UserAccount {
+public class UserAccount extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,12 +34,6 @@ public class UserAccount {
     @Setter private String email;
     @Setter private String nickname;
     @Setter private String memo;
-
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
-    @CreatedBy @Column(nullable = false) private String createdBy;
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
-    @LastModifiedBy @Column(nullable = false) private String modifiedBy;
-
 
     protected UserAccount() {}
 
