68bff1704002543e4aa31841c5956ec0e53e7bee
hegunhee <gunhee0072@gmail.com>
2024-10-28 23:03:45 +0900

    #40 - revert : testContainers 관련 작업을 모두 revert 함
    현재 제품의 CI를 정상적으로 동작하는게 우선이기 때문에 TestContainers 관련 작업은 후에 작업하는게 좋다고 판단해서 TestContainers 관련 작업들을 모두 revert 시킴

--- src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -3,7 +3,6 @@ package com.fastcampus.projectboard.repository;
 import com.fastcampus.projectboard.config.JpaConfig;
 import com.fastcampus.projectboard.domain.Article;
 import com.fastcampus.projectboard.domain.UserAccount;
-import com.fastcampus.projectboard.repository.container.TestContainerConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -36,7 +35,6 @@ class JpaRepositoryTest {
     @Nested
     @DisplayName("실제 DB 테스트")
     @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-    @Import(TestContainerConfig.class)
     class ActualDBTest extends DBTest {
 
         public ActualDBTest(
