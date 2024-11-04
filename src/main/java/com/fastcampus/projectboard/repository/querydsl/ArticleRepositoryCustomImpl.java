3d8e8a4973ad0e00ad98ccb18a0db082bdbb0a59
이건희 <leech9876@naver.com>
2024-11-04 23:25:34 +0900

    #43 - feat : 키워드별 검색을 Service단에서 제어하는것이 아닌 Repository에서 동적 쿼리로 제어
    아직 Service단의 코드는 변경하지 않음
    해당 코드에 대한 검증이 필요해서 Repository 구현만 만들어놓고 테스트코드를 작성함
    
    Service에서 검색어별로 Repository의 메서드를 호출한다면
    여러개의 메서드가 필요하고
    같은 객체를 받는데 검색어별로 호출해야하는 쿼리가 달라진다면 그건 Service단의 책임이 아닌 Repository단의 책임이라고 생각함
    
    테스트는 검색 타입이 없는 전체 조회와
    타입별로 테스트해봄

--- src/main/java/com/fastcampus/projectboard/repository/querydsl/ArticleRepositoryCustomImpl.java
+++ src/main/java/com/fastcampus/projectboard/repository/querydsl/ArticleRepositoryCustomImpl.java
@@ -2,6 +2,13 @@ package com.fastcampus.projectboard.repository.querydsl;
 
 import com.fastcampus.projectboard.domain.Article;
 import com.fastcampus.projectboard.domain.QArticle;
+import com.fastcampus.projectboard.domain.type.SearchType;
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.jpa.impl.JPAQuery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
@@ -22,5 +29,42 @@ public class ArticleRepositoryCustomImpl extends QuerydslRepositorySupport imple
                 .select(article.hashtag)
                 .where(article.hashtag.isNotNull())
                 .fetch();
+    }
+
+    @Override
+    public Page<Article> findBySearchKeyword(SearchType searchType, String searchKeyword, Pageable pageable) {
+        QArticle article = QArticle.article;
+
+        Predicate searchTypePredicate = getPredicateBy(searchType, searchKeyword, article);
+
+        List<Article> contents = from(article)
+                .select(article)
+                .where(searchTypePredicate)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        long count = from(article)
+                .select()
+                .where(searchTypePredicate)
+                .fetchCount();
+
+        return new PageImpl<>(contents, pageable, count);
+    }
+
+    private Predicate getPredicateBy(SearchType searchType, String searchKeyword, QArticle article) {
+        if (searchKeyword == null || searchKeyword.isBlank()) {
+            return null;
+        }
+
+        Predicate result = null;
+        switch (searchType) {
+            case TITLE -> result = article.title.contains(searchKeyword);
+            case CONTENT -> result = article.content.contains(searchKeyword);
+            case ID -> result = article.userAccount.userId.contains(searchKeyword);
+            case NICKNAME -> result = article.userAccount.nickname.contains(searchKeyword);
+            case HASHTAG -> result = article.hashtag.eq(searchKeyword);
+        }
+        return result;
     }
 }
