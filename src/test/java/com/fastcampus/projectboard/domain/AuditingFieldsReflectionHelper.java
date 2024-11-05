0a5c53b691d50d462508b25759c8c136a20b6d61
이건희 <leech9876@naver.com>
2024-11-05 16:52:29 +0900

    #42 - style : AuditingFieldsReflectionHelper의 접근제어자를 public 으로 설정

--- src/test/java/com/fastcampus/projectboard/domain/AuditingFieldsReflectionHelper.java
+++ src/test/java/com/fastcampus/projectboard/domain/AuditingFieldsReflectionHelper.java
@@ -4,17 +4,17 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDateTime;
 
-class AuditingFieldsReflectionHelper {
+public class AuditingFieldsReflectionHelper {
 
     private AuditingFieldsReflectionHelper() {
 
     }
 
-    static void setAuditingFields(AuditingFields entity, int offset, LocalDateTime localDateTime) {
+    public static void setAuditingFields(AuditingFields entity, int offset, LocalDateTime localDateTime) {
         setAuditingFields(entity, offset, localDateTime, 3);
     }
 
-    static void setAuditingFields(AuditingFields entity, int offset, LocalDateTime localDateTime, int modifiedAddMinute) {
+    public static void setAuditingFields(AuditingFields entity, int offset, LocalDateTime localDateTime, int modifiedAddMinute) {
         ReflectionTestUtils.setField(entity, "createdAt", localDateTime.plusMinutes(offset));
         ReflectionTestUtils.setField(entity, "createdBy", "createdBy" + offset);
         ReflectionTestUtils.setField(entity, "modifiedAt", localDateTime.plusMinutes(modifiedAddMinute + offset));
