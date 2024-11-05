57c8504b3069134b349dd4b0e6e2f329c8910d56
이건희 <leech9876@naver.com>
2024-11-05 16:27:49 +0900

    #42 - test : 테스트를 한번만 진행하는것이 아닌 다른값으로 여러번 테스트를 실행

--- src/test/java/com/fastcampus/projectboard/domain/AuditingFieldsEqualsHashcodeTest.java
+++ src/test/java/com/fastcampus/projectboard/domain/AuditingFieldsEqualsHashcodeTest.java
@@ -3,15 +3,20 @@ package com.fastcampus.projectboard.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static com.fastcampus.projectboard.domain.AuditingFieldsReflectionHelper.setAuditingFields;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 @DisplayName("[AuditingField] AuditingFields equals hashcode 메서드 테스트")
 public class AuditingFieldsEqualsHashcodeTest {
@@ -35,15 +40,26 @@ public class AuditingFieldsEqualsHashcodeTest {
     }
 
     @DisplayName("모든 필드값이 같은 2개의 객체 중복 비교")
-    @Test
-    void givenTwoSameArticle_when_thenSameArticle() {
+    @MethodSource
+    @ParameterizedTest(name = "현재 offset {0}")
+    void givenTwoSameArticle_when_thenSameArticle(int offset) {
         // given
-        Article article1 = createArticleWithOffset(1);
-        Article article2 = createArticleWithOffset(1);
+        Article article1 = createArticleWithOffset(offset);
+        Article article2 = createArticleWithOffset(offset);
 
         // when & then
         assertThat(article1).isEqualTo(article2);
         assertThat(article1.hashCode()).isEqualTo(article2.hashCode());
+    }
+
+    static Stream<Arguments> givenTwoSameArticle_when_thenSameArticle() {
+        return Stream.of(
+                arguments(1),
+                arguments(2),
+                arguments(3),
+                arguments(4),
+                arguments(5)
+        );
     }
 
     @DisplayName("AuditingField만 다른 2개의 객체 중복 비교")
